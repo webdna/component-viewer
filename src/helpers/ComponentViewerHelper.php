0f48b1580175c8df601d2cb29eaeba803e9b9d23
Samuel Birch <sam.birch@gmail.com>
2023-12-31 16:31:44 +0000

    side bar to use label from config

--- src/helpers/ComponentViewerHelper.php
+++ src/helpers/ComponentViewerHelper.php
@@ -147,7 +147,7 @@ class ComponentViewerHelper
                     }
                 } else {
                     $buttons[] = [
-                        'label' => $componentName,
+                        'label' => $config['label'] ?? $componentName,
                     ];
                 }
 
